a48cdf7a5550626e6f2e27471c91b2aa38fcaea7
Leidiane <leidiane.elisa@inatel.br>
2022-03-20 19:25:39 -0300

    Add more informations at Personal page

--- src/pages/Personal/personal.js
+++ src/pages/Personal/personal.js
@@ -1,10 +1,23 @@
 import Layout from '../../components/layout/layout';
 import ProfileHeader from '../../components/profileHeader';
+import { Paragraph, Pill, SectionTitle } from '../../styles';
 
 const Personal = ({ profile }) => {
 	return (
 		<Layout>
 			<ProfileHeader profile={profile} />
+			<div>
+				<SectionTitle>Sobre Mim</SectionTitle>
+				<Paragraph>{profile.basics.summary}</Paragraph>
+			</div>
+			<div>
+				<SectionTitle>Conhecimentos</SectionTitle>
+				<div>
+					{profile.skills.map(skill => (
+						<Pill key={skill.name}>{skill.name}</Pill>
+					))}
+				</div>
+			</div>
 		</Layout>
 	);
 };
